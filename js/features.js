c36fcd9f87881bcbd1661534c8143d89c68ef49f
Ivy <ouyayuyi@gmail.com>
2026-08-16 23:39:29 +0800

    Update features.js
    后台保活音频修改

--- js/features.js
+++ js/features.js
@@ -186,7 +186,7 @@
 
 (function() {
     var KEY = 'keepaliveAudioEnabled';
-    var SRC = 'assets/audio/silence.m4a';
+    var SRC = 'assets/audio/silence.mp3';
     var _audio = null;
     var _unlockBound = false;
 
@@ -200,6 +200,7 @@
         _audio.preload = 'auto';
         _audio.addEventListener('play',  function(){ _setUI(true);  });
         _audio.addEventListener('pause', function(){ _setUI(false); });
+        window._debugKeepaliveAudio = _audio;
         return _audio;
     }
 
